33b4f7744e1ea93d49b45f483d86a0f7319f07ff
Darshil Patel <darshil.32@gmail.com>
2021-04-13 00:57:28 +0000

    Added the genres dictionary and checking if username works

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
-genreVotes = {}
+genreVotes = {'28' : 'Action', '12' : 'Adventure', '16' : 'Animation', '32' : 'Comedy', 
+              '80' : 'Crime', '99' : 'Documentary', '18' : 'Drama', '10751' : 'Family', 
+              '14' : 'Fantasy', '36' : 'History', '27' : 'Horror', '10402' : 'Music', 
+              '9648' : 'Mystery', '10749' : 'Romance', '878' : 'Science Fiction', '53' : 'Thriller'}
 
 app = Flask(__name__, static_folder='./build/static')
 
