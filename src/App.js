5b56ef0d9596975617b89de093635a5cadf8e8d7
Dezrianna <dac57@njit.edu>
2021-04-15 04:42:43 +0000

    Updated the check for success/ fail on login

--- src/App.js
+++ src/App.js
@@ -8,25 +8,38 @@ import GoogleLogin from 'react-google-login';
 const socket = io(); // Connects to socket connection
 const arr = ['','']
 function App() {
-  console.log(process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID)
   const [messages, setMessages] = useState([]); // State variable, list of messages
   const inputRef = useRef(null); // Reference to <input> element
+  const [success, setSucc] = useState(false);
   const [info, setInfo] = useState(arr);
   // Upon user login print out user's name and email
+
    useEffect(()=>{
         socket.on('email',(data)=>{
+            console.log(data)
             setInfo(data);
         });
     },[]);
   
   function onLoginButton(response) {
-    console.log(response.profileObj.name)
-    console.log(response.profileObj.email);
-    let infoNE = [response.profileObj.name, response.profileObj.email]
-    socket.emit('email', infoNE)
-    
+    try{
+      console.log(response.profileObj.name)
+      console.log(response.profileObj.email);
+      let infoNE = [response.profileObj.name, response.profileObj.email]
+      socket.emit('email', infoNE)
+      setSucc(true)
+    }
+    catch(err){
+      window.alert('Login Failed. Please Try Again')
+    }
   }
-
+if (success === true){
+  return(
+    <div>
+      <Genres />
+    </div>
+    );
+}
   // The function inside useEffect is only run whenever any variable in the array
   // (passed as the second arg to useEffect) changes. Since this array is empty
   // here, then the function will only run once at the very beginning of mounting.
