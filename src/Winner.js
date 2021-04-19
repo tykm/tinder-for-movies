96a0c6a80208cd0d7979abe4b256d8f9dabf610b
Dezrianna <dac57@njit.edu>
2021-04-19 21:29:31 +0000

    This should be all of the info

--- src/Winner.js
+++ src/Winner.js
@@ -1,27 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { socket } from './App.js';
 export function Winner() {
+    const [info,setInfo] = useState([])
     useEffect(()=>{
         socket.on('movieWinner',(data)=>{
-            console.log(data)
+            setInfo(data)
         });
     },[]);
+    //[name, number of like, rating, picture, dezcription]
   // will need props to get information from the other component
     // let changes while const will not be reassigned
-    const movieL = ["The array of movies received to Rec.js"]
-    const voteL = ["However the votes for each movie will be sent"]
+    const movieL = info[0]
+    const voteL = info[2]
+    const pic = "\'"+info[3]+"\'"
     let pos = 0 //The position of the movie with the highest rating; will be needed for the next sprint
     // { props.array } for the arrays and a useEffect to receive API information from the back-end
     // probably need to resize the image and create a stylesheet
     return (
         <div>
-            <p> Movie name here... {movieL[pos]}</p>
-            <img src="www.defareallink.com" alt="Movie Poster" />
+            <p> Movie name here... {movieL}</p>
+            <img src={pic} alt="Movie Poster" />
             <p> 
-                User rating: {voteL[pos]}<br/>
-                Movie Description <br/>
-                <a href="www.defareallink.com"> Movie review </a> <br/>
-                <a href="www.defareallink.com"> Movie trailer </a>
+                User likes: {info[1]}<br/>
+                User rating: {voteL}<br/>
+                Movie Description: <br/>
+                {info[4]} <br/>
             </p>
         </div>
     );
