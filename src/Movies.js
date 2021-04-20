fd88518004641239251b58638fbf7406783f6b6b
Darshil Patel <darshil.32@gmail.com>
2021-04-20 23:28:54 +0000

    timer set to 45 seconds

--- src/Movies.js
+++ src/Movies.js
@@ -8,7 +8,7 @@ export function Movies() {
   const [isMoviePage, setMoviePage] = useState(false);
   const [timerEnd, setTimerEnd] = useState(false);
   const expiryTimestamp = new Date();
-  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 10);
+  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 45);
   const { seconds, isRunning } = useTimer({
     expiryTimestamp,
     autoStart: true,
