9bf669cb1c8f7f5f78008fcfc64fa436fceaf839
Mahi Gada <ec2-user@ip-172-31-30-45.ec2.internal>
2021-04-20 05:36:46 +0000

    Removed a warning for ESLint

--- src/Genres.js
+++ src/Genres.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { socket } from "./App.js";
 import { Movies } from "./Movies.js";
 import { useTimer } from "react-timer-hook";
